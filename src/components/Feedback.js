f4e901dbdc231cb1bf9845c3c43ede4523cceb08
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-08-09 17:12:18 -0300

    Finalizado exercicio 15

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Feedback extends Component {
   render() {
-    const {score, assertions} = this.props;
+    const { score, assertions, history } = this.props;
     return (
       <div data-testid="feedback-text">
         <Header />
         <h4 data-testid="feedback-total-score">{score}</h4>
         <h5 data-testid="feedback-total-question">{assertions}</h5>
+        <button
+          type="button"
+          onClick={ () => { history.push('/'); } }
+          data-testid="btn-play-again"
+        >
+          Play Again
+        </button>
       </div>
     );
   }
@@ -20,7 +28,7 @@ const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
 });
 
-Feedback.propTypes ={
+Feedback.propTypes = {
   score: PropTypes.number,
   assertions: PropTypes.number,
 }.isRequired;
