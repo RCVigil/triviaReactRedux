baf4372d00a9dc60e36395a20405b612a11dd05c
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-08-09 16:54:19 -0300

    Finalizado exercício -14

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from './Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const {score, assertions} = this.props;
     return (
-      <h1 data-testid="feedback-text">
+      <div data-testid="feedback-text">
         <Header />
-      </h1>
+        <h4 data-testid="feedback-total-score">{score}</h4>
+        <h5 data-testid="feedback-total-question">{assertions}</h5>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
