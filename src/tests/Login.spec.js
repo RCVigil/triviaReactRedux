023ed2ed9dbb09ec7c770f1b39208327ea20aa56
VictorSilva27 <victoradaosilva2@gmail.com>
2022-08-10 16:46:25 -0300

    Teste Login

--- src/tests/Login.spec.js
+++ src/tests/Login.spec.js
@@ -5,11 +5,6 @@ import App from '../App';
 import Login from '../components/Login';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 
-const INITIAL_STATE = { player : {
-    name: 'valido',
-    gravatarEmail: 'hehe'
-}}
-
 describe('Testar Login', () => {
     it('Se é desabilitado o botão play, quando o inputName ou IputEmail estiver vazio', () => {
         renderWithRouterAndRedux(<App />);
@@ -29,20 +24,23 @@ describe('Testar Login', () => {
         expect(btnPlay).toHaveProperty("disabled", false);
     });
     
-    it('Se é habilitado o botão play, quando o inputName ou IputEmail estiverem', () => {
-        renderWithRouterAndRedux(<App />);
+    it('Se ao clicar no botão play, a função "handleButton" é chamada', () => {
+        const STATE = {token: '6a190b20f637bae6d0c4841364b563862044b3e304258380af2581d69728e1c9'};
+        jest.spyOn(global, 'fetch');
+        global.fetch.mockResolvedValue({
+            json: jest.fn().mockResolvedValue(STATE),
+        });
+        const { history } = renderWithRouterAndRedux(<App />);
         const inputEmail = screen.getByTestId('input-gravatar-email');
         expect(inputEmail).toBeInTheDocument();
         const inputName = screen.getByTestId('input-player-name');
         expect(inputName).toBeInTheDocument();
         const btnPlay = screen.getByTestId('btn-play');
         expect(btnPlay).toBeInTheDocument();
-
         userEvent.type(inputName, 'Valido');
         userEvent.type(inputEmail, 'Valido');
-        expect(btnPlay).toHaveProperty("disabled", false);
-    });
-    it('Se ao clicar no botão play, a função "handleButton" é chamada', () => {
-        renderWithRouterAndRedux(<App />);
+
+        userEvent.click(btnPlay);
+
     });
 });
